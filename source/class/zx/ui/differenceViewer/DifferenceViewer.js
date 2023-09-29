c3ab22f4ad37352cb8f4fabe5ab51646e5829133
Patryk Milinowski <pmilinowski@vmn.digital>
2023-09-29 17:41:51 +0100

    oops

--- source/class/zx/ui/differenceViewer/DifferenceViewer.js
+++ source/class/zx/ui/differenceViewer/DifferenceViewer.js
@@ -32,6 +32,8 @@ qx.Class.define("zx.ui.differenceViewer.DifferenceViewer", {
     appearance: {
       init: "difference-viewer",
       refine: true
+    },
+
     /**Whether to show or hide the controls */
     showColumnControls: {
       check: "Boolean",
